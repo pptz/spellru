e4a556995f2fdb6069bb88e60952d40702210fbd
Denis Begun <pptzpp@gmail.com>
2025-03-17 21:44:55 +0200

    make hints hideable

--- src/spellru.jsx
+++ src/spellru.jsx
@@ -315,7 +315,7 @@ const RussianWordGame = () => {
 
       <div className="flex space-x-2 mt-4">
         <button
-          onClick={() => setShowHints(true)}
+          onClick={() => setShowHints(!showHints)}
           className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600"
         >
           Подсказки
