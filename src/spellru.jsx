871ea33411baa121a26d11bf860a4a6360acf469
Denis Begun <pptzpp@gmail.com>
2025-03-17 21:31:27 +0200

    hints hover color

--- src/spellru.jsx
+++ src/spellru.jsx
@@ -313,7 +313,7 @@ const RussianWordGame = () => {
       <div className="flex space-x-2 mt-4">
         <button
           onClick={() => setShowHints(true)}
-          className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-red-600"
+          className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600"
         >
           Подсказки
         </button>
